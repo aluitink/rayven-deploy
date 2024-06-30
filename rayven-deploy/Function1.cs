1ae9f74cf68f0d8b2d206f5bb06c424ae260d1f6
aluitink <aluitink@gmail.com>
2024-06-30 08:35:09 -0500

    deploy with static site client?=

--- rayven-deploy/Function1.cs
+++ rayven-deploy/Function1.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace rayven_deploy
 {
     public class Function1
     {
+
+        public static bool IsLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
         private readonly ILogger<Function1> _logger;
 
         public Function1(ILogger<Function1> logger)
@@ -15,10 +21,42 @@ namespace rayven_deploy
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            _logger.LogInformation($"Environment Is Linux {IsLinux()}");
+
+
+            var windowsDlPath = "https://swalocaldeploy.azureedge.net/downloads/1.0.027044/windows/StaticSitesClient.exe";
+            var linuxDlPath = "https://swalocaldeploy.azureedge.net/downloads/1.0.027044/linux/StaticSitesClient";
+
+            var dlPath = windowsDlPath;
+            if (IsLinux())
+                dlPath = linuxDlPath;
+
+            var tempPath = Path.GetTempPath();
+
+            HttpClient client = new HttpClient();
+            var downloadedFile = await client.GetStreamAsync(dlPath);
+            var staticSitesClient = Path.Join(tempPath, "StaticSitesClient");
+            if (File.Exists(staticSitesClient))
+                File.Delete(staticSitesClient);
+            using (var fs = new FileStream(staticSitesClient, FileMode.CreateNew))
+            {
+                await downloadedFile.CopyToAsync(fs);
+            }
+
+            var process = Process.Start(staticSitesClient, "upload --help");
+            var sb = new StringBuilder();
+            process.OutputDataReceived += (sender, args) => {
+                sb.AppendLine(args.Data);
+            };
+            process.WaitForExit();
+
+            _logger.LogDebug(sb.ToString());
+
+            return new OkObjectResult(sb.ToString());
         }
     }
 }
